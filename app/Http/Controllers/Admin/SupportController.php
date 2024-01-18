e7253eace5570d1d466be9750f8664c6667c6bc5
altafin <altafin@uel.br>
2024-01-18 13:27:01 -0300

    Aula 14

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -39,4 +39,27 @@ class SupportController extends Controller
         return view('admin/supports/show', compact('support'));
     }
 
+    public function edit(Support $support, string|int $id)
+    {
+        if (!$support = $support->where('id', $id)->first()) {
+            return back();
+        }
+
+        return view('admin/supports/edit', compact('support'));
+    }
+
+    public function update(Request $request, Support $support, string|int $id)
+    {
+        if (!$support = $support->find($id)) {
+            return back();
+        }
+
+        $support->update($request->only([
+            'subject', 'body'
+        ]));
+
+        return redirect()->route('supports.index');
+
+    }
+
 }
