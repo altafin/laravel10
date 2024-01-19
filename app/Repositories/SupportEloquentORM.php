7629a6d0fa29516eb1be9a2c67c99b712f33a9ad
altafin <altafin@uel.br>
2024-01-19 10:51:42 -0300

    Aula 22

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -1,29 +1,50 @@
 <?php
 
-class SupportEloquentORM implements \App\Repositories\SupportRepositoryInterface
+namespace App\Repositories;
+
+use App\DTO\CreateSupportDTO;
+use App\DTO\UpdateSupportDTO;
+use App\Models\Support;
+use stdClass;
+class SupportEloquentORM implements SupportRepositoryInterface
 {
+    public function __construct(
+        protected Support $model
+    ) {}
 
     public function getAll(string $filter = null): array
     {
-        // TODO: Implement getAll() method.
+        return $this->model
+            ->where(function ($query) use ($filter) {
+                if ($filter) {
+                    $query->where('subject', $filter);
+                    $query->orWhere('body', 'like', "%$filter%");
+                }
+            })
+            ->all()
+            ->toArray();
     }
 
     public function findOne(string $id): stdClass|null
     {
-        // TODO: Implement findOne() method.
+        $support = $this->model->find($id);
+        if (!$support) {
+            return null;
+        }
+        return (object) $support->toArray();
     }
 
     public function delete(string $id): void
     {
-        // TODO: Implement delete() method.
+        $this->model->findOrFail($id)->delete();
     }
 
-    public function new(\App\DTO\CreateSupportDTO $dto): stdClass
+    public function new(CreateSupportDTO $dto): stdClass
     {
         // TODO: Implement new() method.
     }
 
-    public function update(\App\DTO\UpdateSupportDTO $dto): stdClass|null
+    public function update(UpdateSupportDTO $dto): stdClass|null
     {
         // TODO: Implement update() method.
     }
