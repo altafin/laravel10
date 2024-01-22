46e87c175d2e83e3a1fc0f47a13f4b09485da8e3
altafin <altafin@uel.br>
2024-01-22 11:50:44 -0300

    Aula 36

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -39,7 +39,9 @@ class SupportController extends Controller
         $this->service->new(
             CreateSupportDTO::makeFromRequest($request)
         );
-        return redirect()->route('supports.index');
+        return redirect()
+            ->route('supports.index')
+            ->with('message', 'Cadastrado com sucesso');
     }
 
     public function show(string $id)
@@ -70,7 +72,9 @@ class SupportController extends Controller
             return back();
         }
 
-        return redirect()->route('supports.index');
+        return redirect()
+            ->route('supports.index')
+            ->with('message', 'Atualizado com sucesso');
 
     }
 
--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -3,33 +3,11 @@
 @section('title', 'Fórum')
 
 @section('header')
-<h1>Listagem dos Suportes</h1>
+@include('admin.supports.partials.header')
 @endsection
 
 @section('content')
-<a href="{{ route('supports.create') }}">Criar Dúvida</a>
-
-<table>
-    <thead>
-        <th>assunto</th>
-        <th>status</th>
-        <th>descrição</th>
-        <th></th>
-    </thead>
-    <tbody>
-        @foreach($supports->items() as $support)
-            <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ getStatusSupport($support->status) }}</td>
-                <td>{{ $support->body }}</td>
-                <td>
-                    <a href="{{ route('supports.show', $support->id) }}">Ir</a>
-                    <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+@include('admin.supports.partials.content')
 
 <x-pagination
     :paginator="$supports"
